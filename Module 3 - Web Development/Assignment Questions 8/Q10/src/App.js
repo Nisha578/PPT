4349625e5eb560a882903f66004b61ae7270f76b
Nisha Yadav <vatsnishaa@gmail.com>
2023-08-03 00:25:10 -0700

    Add files via upload

---
+++ Module 3 - Web Development/Assignment Questions 8/Q10/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SimpleWebsite from './Components/SimpleWebsite';
+
+function App() {
+  return (
+   <div className='App'>
+    <SimpleWebsite/>
+   </div>
+  );
+}
+
+export default App;
